5fef3250ba56403e97d6df3e3ee140ad7e148839
5ekastan <151753486+nasirovx@users.noreply.github.com>
2023-11-30 08:42:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-const API = "2401b88c6f203d44d86930611fc5e75a";
-let form = document.getElementById('form');
-let citys = document.getElementById("city");
-let temp = document.getElementById("temp");
-let iconca = document.getElementById("icon");
-let speed = document.getElementById("speed");
-let btn = document.getElementById("btn");
-let cityInput = document.getElementById("cityInput");
+const API_KEY = "2401b88c6f203d44d86930611fc5e75a";
+const form = document.getElementById('form');
+const cityElement = document.getElementById("city");
+const tempElement = document.getElementById("temp");
+const iconElement = document.getElementById("icon");
+const descriptionElement = document.getElementById("description");
+const btn = document.getElementById("btn");
+const cityInput = document.getElementById("cityInput");
 
 btn.addEventListener("click", () => {
-  let city = cityInput.value;
-  getWeather(city);
-  cityInput.value = ''
+    const city = cityInput.value;
+    getWeather(city);
+    cityInput.value = '';
 });
 
-const getWeather = (city) =>{
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2401b88c6f203d44d86930611fc5e75a&units=metric`;
-  fetch(apiURL)
-    .then(response => response.json())
-    .then(data => {
-      citys.textContent = data.name;
-      temp.textContent = `${data.main.temp}°C`;
-      iconca.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-      speed.textContent = data.weather[0].description;
-    })
-    .catch(error => {
-      console.log("Произошла ошибка при получении данных о погоде:", error);
-    });
-}
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let city = cityInput.value;
+    const city = cityInput.value;
     getWeather(city);
-    cityInput.value = ''
-  });
+    cityInput.value = '';
+});
+
+const getWeather = (city) => {
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            cityElement.textContent = data.name;
+            tempElement.textContent = `${data.main.temp}°C`;
+            iconElement.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+        });
+};
